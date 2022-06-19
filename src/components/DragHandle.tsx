b503583b1c58a6659fd6bbcc3977603907e33956
Mathieu Tinoco <tinoco.mathieu@gmail.com>
2022-06-19 21:10:30 +0200

    fixed some appearance details

--- src/components/DragHandle.tsx
+++ src/components/DragHandle.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import dragIcon from '../assets/dragIcon.svg';
 
-export default function DragHandle () {
+export default function DragHandle() {
   return (
-    <div> 
-      <svg  width="16" height="16" >
+    <div>
+      {/* <img src={dragIcon} alt="delete" /> */}
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
         <path d="M7 2a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM7 5a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM7 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm-3 3a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm-3 3a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
       </svg>
     </div>
