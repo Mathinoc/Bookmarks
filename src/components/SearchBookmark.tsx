97b3d05dc5c0937224fa8c601e4f7ff2c5ba6e7d
Mathieu Tinoco <tinoco.mathieu@gmail.com>
2022-07-15 17:35:19 +0200

    added styling

--- src/components/SearchBookmark.tsx
+++ src/components/SearchBookmark.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import "../styles/SearchBookmark.css";
+import search from "../assets/search.svg";
 
 export default function SearchBookmark({ searchString, setSearchString }: { searchString: string | undefined, setSearchString: React.Dispatch<React.SetStateAction<string | undefined>> }) {
   return (
     <div className="SearchBookmark" >
+      <button>
+        <img src={search}/>
+      </button>
       <input
         type="search"
         placeholder="Recherche..."
